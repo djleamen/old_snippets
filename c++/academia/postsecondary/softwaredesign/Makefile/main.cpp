0e13ffe4a6f7e958e6eaf1160a7e4dc726ab3c7d
DJ <dj.leamen@ontariotechu.net>
2025-03-15 16:36:02 -0400

    Added activity9b from softdesign

---
+++ c++/academia/postsecondary/softwaredesign/Makefile/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "functions.h"
+
+int main() {
+    std::cout << "Sum of 3 and 5: " << add(3, 5) << std::endl;
+    std::cout << "Product of 3 and 5: " << multiply(3, 5) << std::endl;
+    return 0;
+}
